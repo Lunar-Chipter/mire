93211ccb6541a3668058400b70062c9d7ab9c2f6
Lunar-Chipter <lunarchipter@gmail.com>
2025-11-27 21:00:33 +0800

    Add files via upload

--- sampler/sampler.go
+++ sampler/sampler.go
@@ -1,1 +1,45 @@
+package sampler
 
+import (
+	"context"
+	"sync"
+	"sync/atomic"
+	"mire/core"
+)
+
+// LogSampler defines the interface for a logger that can be sampled.
+type LogSampler interface {
+	Log(ctx context.Context, level core.Level, msg []byte, fields map[string]interface{})
+}
+
+// SamplingLogger provides log sampling to reduce volume
+type SamplingLogger struct {
+	processor LogSampler
+	rate      int
+	counter   int64
+	mu        sync.Mutex
+}
+
+// NewSamplingLogger creates a new SamplingLogger
+func NewSamplingLogger(processor LogSampler, rate int) *SamplingLogger {
+	return &SamplingLogger{
+		processor: processor,
+		rate:      rate,
+	}
+}
+
+// ShouldLog determines if a log should be recorded based on sampling rate
+func (sl *SamplingLogger) ShouldLog() bool {
+	if sl.rate <= 1 {
+		return true
+	}
+	counter := atomic.AddInt64(&sl.counter, 1)
+	return counter%int64(sl.rate) == 0
+}
+
+// Log logs a message if it passes the sampling rate.
+func (sl *SamplingLogger) Log(ctx context.Context, level core.Level, msg []byte, fields map[string]interface{}) {
+    if sl.ShouldLog() {
+        sl.processor.Log(ctx, level, msg, fields)
+    }
+}
